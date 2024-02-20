00e9a69a9d6a5b275ac88c5c269435f5fb28a1c7
Israel Zacarias <Israel Zacarias@DESKTOP-56CI4RG>
2024-02-20 00:48:22 -0600

    Agregar archivos de proyecto.

---
+++ Practica Unit 2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+using System;
+
+class PhysicsConverter
+{
+    static void Main()
+    {
+        // Get user input for volume in liters
+        Console.Write("Enter volume in liters: ");
+        double litersVolume = Convert.ToDouble(Console.ReadLine());
+
+        // Convert volume to gallons using the formula: gallons = liters * 0.264172
+        double gallonsVolume = ConvertLitersToGallons(litersVolume);
+
+        // Display the converted volume
+        Console.WriteLine($"The volume in gallons is: {gallonsVolume} gallons");
+    }
+
+    // Method to convert liters to gallons
+    // Formula: gallons = liters * 0.264172
+    static double ConvertLitersToGallons(double litersVolume)
+    {
+        return litersVolume * 0.264172;
+    }
+}
